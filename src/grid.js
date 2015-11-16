8b7f6ee27e127ebf67695c9ef4da62aa90fa6138
Simiao Carvalho <simiaosimis@gmail.com>
2015-11-16 21:59:42 -0200

    Zoom and active/deactive grid

--- src/grid.js
+++ src/grid.js
@@ -3,26 +3,32 @@ Grid.prototype.constructor=Grid;
 
 function Grid() {
   Drawable.call(this);
+  this.width = apo.canvas.width;
+  this.height = apo.canvas.height;
 
   this.active = true;
   this.step = 20;
 
   this.draw = function(){
-  	var width = apo.canvas.width;
-  	var height = apo.canvas.height;
+  	apo.ctx.clearRect(0, 0, this.width, this.height);
   	apo.ctx.lineWidth = 0.5;
   	apo.ctx.strokeStyle = "blue";
-  	for (var i = 0; i < height; i+=this.step) {
+  	for (var i = 0; i < this.height; i+=this.step) {
   		apo.ctx.beginPath();
   		apo.ctx.moveTo(0,i);
-  		apo.ctx.lineTo(width,i);
+  		apo.ctx.lineTo(this.width,i);
   		apo.ctx.stroke();
   	}
-  	for (var i = 0; i < width; i+=this.step) {
+  	for (var i = 0; i < this.width; i+=this.step) {
   		apo.ctx.beginPath();
   		apo.ctx.moveTo(i,0);
-  		apo.ctx.lineTo(i,height);
+  		apo.ctx.lineTo(i,this.height);
   		apo.ctx.stroke();
   	}
+  }
+
+  this.changeState = function(){
+  	this.active = !this.active;
+  	mouseOverCanvas = true;
   }
 }
