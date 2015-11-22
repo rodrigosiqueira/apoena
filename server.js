cdd21aa38f4ac2fb8d9fa5f0ffb2982854a2f34c
Gust <darksshades@hotmail.com>
2015-11-22 20:08:02 -0200

    Add node structure

---
+++ server.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+var finalhandler = require('finalhandler');
+var serveStatic = require('serve-static');
+
+var serve = serveStatic("./");
+
+var server = http.createServer(function(req, res) {
+  var done = finalhandler(req, res);
+  serve(req, res, done);
+});
+
+console.log("Listening on port 4000");
+server.listen(4000);
